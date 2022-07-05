dfed4ccc402c84958a6fb3753e92966eb887c990
Mohamed Amr <40462023+MoAmrYehia@users.noreply.github.com>
2022-07-05 20:45:49 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,28 +1,19 @@
 from PIL import Image
-from keras.preprocessing import image
-from keras.applications.vgg16 import VGG16, preprocess_input
-from keras.models import Model
-import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.models import Model
 import numpy as np
 import pandas as pd
-import boto3
+import tensorflow as tf
 
-
-S3_BUCKET = 'autodraw' 
-folder = 'features/'
-file_name = "feature_extraction_6.csv"
-
-s3 = boto3.client('s3',
-         aws_access_key_id='AKIAYHBCWEYTNPL6AYV4',
-         aws_secret_access_key= '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')
-
-obj = s3.get_object(Bucket= S3_BUCKET, Key= folder+ file_name)
+file_name = "https://autodraw.s3.us-west-2.amazonaws.com/features/resized_features_2.csv"
+weights = "vgg16_weights_tf_dim_ordering_tf_kernels.h5"
 
 class FeatureExtractor:
     def __init__(self):
         
         # Use VGG-16 as the architecture and ImageNet for the weight
-        base_model = VGG16(weights='imagenet')
+        base_model = VGG16(weights=weights)
         # Customize the model to return features from fully-connected layer
         self.model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
         
@@ -50,7 +41,7 @@ def perdict_img(file):
     # Extract its features
     query = fe.extract(img)
   
-    features = pd.read_csv(obj['Body'])
+    features = pd.read_csv(file_name).drop(columns=["Unnamed: 0", "Unnamed: 0.1"])
 
     # Calculate the similarity (distance) between images
     features_data = features.drop(columns = ['image'])
@@ -64,34 +55,9 @@ def perdict_img(file):
   
     lookalike_imgs = features.iloc[ids,:]['image']
 
-    scores = pd.DataFrame({'image': lookalike_imgs,
-                        'score': dists[ids]})
-
+    scores = pd.DataFrame({'image': lookalike_imgs})
 
     scores = scores.reset_index(drop=True) 
-
     scores = scores.to_json(orient='index')
 
-
-    # scores is a dataframe object contrain 2 columns image name and its score
-    # scores contains the 10 least score images according to input image
-    # |____image_name______|____score_____|  
-    # |______456.png_______|____8.235_____| 
-    # |______789.png_______|____7.258_____| 
-    # |______752.png_______|____7.123_____| 
-    # |______982.png_______|____6.123_____| 
-
-    # Visualize the result
-    # axes=[]
-    # fig=plt.figure(figsize=(8,8))
-    # for a in range(1*10): #in case we want 20 images --> for a in range(1*20)
-    #     score = scores['score'][a]
-    #     axes.append(fig.add_subplot(2, 5, a+1)) # axes.append(fig.add_subplot(2, 10, a+1))
-    #     subplot_title=str(scores['image'][a])
-    #     axes[-1].set_title(subplot_title)  
-    #     plt.axis('off')
-    #     plt.imshow(Image.open(Image_Data_Path+ 'dataset/'+ scores['image'][a]))
-    # fig.tight_layout()
-    # plt.show()
     return scores
-
